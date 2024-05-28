47196f532357616ad1b287adc07acda7c4be5049
Mert Özcan <iammertozcan@gmail.com>
2024-05-28 19:06:07 +0300

    Added currency conversion functionality and UI updates

--- MoneyMorphApp/MoneyMorphApp/ViewController.m
+++ MoneyMorphApp/MoneyMorphApp/ViewController.m
@@ -6,13 +6,18 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UIButton *convertButton;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic) NSArray *data;
+
 @end
 
 @implementation ViewController
@@ -27,6 +32,14 @@
     
     // Optional: Register a default UITableViewCell class for reuse
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+}
+
+- (IBAction)buttonTapped:(id)sender {
+    self.convertButton.enabled = NO;
+    
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
 }
 
 // UITableViewDataSource Methods
@@ -46,6 +59,48 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSLog(@"Selected row: %ld", (long)indexPath.row);
+}
+
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies {
+    self.convertButton.enabled = YES;
+    
+    double inputValue = [self.inputField.text doubleValue];
+        
+        // Calculate the converted values
+        double euroValue = inputValue * currencies.EUR;
+        double jpyValue = inputValue * currencies.JPY;
+        double bgnValue = inputValue * currencies.BGN;
+        double czkValue = inputValue * currencies.CZK;
+        double dkkValue = inputValue * currencies.DKK;
+        double gbpValue = inputValue * currencies.GBP;
+        double hufValue = inputValue * currencies.HUF;
+        double plnValue = inputValue * currencies.PLN;
+        double ronValue = inputValue * currencies.RON;
+        double sekValue = inputValue * currencies.SEK;
+        double chfValue = inputValue * currencies.CHF;
+        double inrValue = inputValue * currencies.INR;
+        double mxnValue = inputValue * currencies.MXN;
+        
+        // Update the data array with the new values
+        self.data = @[
+            [NSString stringWithFormat:@"%.2f EUR", euroValue],
+            [NSString stringWithFormat:@"%.2f JPY", jpyValue],
+            [NSString stringWithFormat:@"%.2f BGN", bgnValue],
+            [NSString stringWithFormat:@"%.2f CZK", czkValue],
+            [NSString stringWithFormat:@"%.2f DKK", dkkValue],
+            [NSString stringWithFormat:@"%.2f GBP", gbpValue],
+            [NSString stringWithFormat:@"%.2f HUF", hufValue],
+            [NSString stringWithFormat:@"%.2f PLN", plnValue],
+            [NSString stringWithFormat:@"%.2f RON", ronValue],
+            [NSString stringWithFormat:@"%.2f SEK", sekValue],
+            [NSString stringWithFormat:@"%.2f CHF", chfValue],
+            [NSString stringWithFormat:@"%.2f INR", inrValue],
+            [NSString stringWithFormat:@"%.2f MXN", mxnValue]
+        ];
+        
+        // Reload the table view to reflect the updated data
+        [self.tableView reloadData];
 }
 
 @end
